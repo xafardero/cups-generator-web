932876d4a71facaebb32189a4510cae2cdcc143f
Xavier Mabras <xavier.mabras@holaluz.com>
2015-02-24 18:20:53 +0100

    Added more things

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,23 @@
-<!-- Stored in resources/views/layouts/master.blade.php -->
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tools</title>
+        <!-- Fonts -->
+        <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    </head>
     <body>
         @section('sidebar')
-            This is the master sidebar.
+        This is the master sidebar.
         @show
 
         <div class="container">
             @yield('content')
         </div>
+        <!-- Scripts -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     </body>
 </html>
