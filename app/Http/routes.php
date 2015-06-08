4a9745baf239cb4c654d763c09b8610b21ff3c12
Xavier Mabras <xafardero@gmail.com>
2015-06-08 10:20:23 +0000

    routes.php edited online with Bitbucket

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,23 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
+Route::resource('/', 'CupsController@index');
 Route::resource('cups', 'CupsController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
