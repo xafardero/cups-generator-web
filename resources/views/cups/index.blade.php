dc8f4f20a9276ecb3ae43c4001121e46beb22615
Xavier Mabras <xavier.mabras@holaluz.com>
2015-09-28 12:00:20 +0200

    Added Wiki info :D

--- resources/views/cups/index.blade.php
+++ resources/views/cups/index.blade.php
@@ -2,15 +2,23 @@
 
 @section ('content')
 <div class="row">
-    <h1 class="text-center">Cups</h1>
-    <h2 class="text-center">Generator</h2>
-        <form class="col-md-6 col-md-offset-3 form-horizontal">
-          <div class="form-group">
-            <input name="cups" type="text" value="{!! $cups; !!}" class="form-control text-center">
-          </div>
-          <div class="form-group">
-            <a href="" class="btn btn-success text-center form-control">Generate</a>
-          </div>
+    <div class="col-md-6 col-md-offset-3 ">
+        <h1 class="text-center">CUPS</h1>
+        <h2 class="text-center">Generador aleatorio</h2>
+        <form class="form-horizontal">
+            <div class="form-group">
+                <input name="cups" type="text" value="{!! $cups; !!}" class="form-control text-center">
+            </div>
+            <div class="form-group">
+                <a href="" class="btn btn-success text-center form-control">Generar</a>
+            </div>
         </form>
+        <blockquote id="wiki">
+            <p>
+                El CUPS (Código Universal del Punto de Suministro), en España, es un código único e identificador de un punto de suministro de energía, ya sea de electricidad o gas canalizado.             Se creó con el objetivo de identificar cada suministro ante compañías suministradoras, distribuidoras, comercializadoras, administración estatal, comunidades autónomas.. etc. Actualmente es muy útil  (...)
+            </p>
+            <footer> <a href="https://es.wikipedia.org/wiki/C%C3%B3digo_Universal_de_Punto_de_Suministro"><cite title="Source Title">Wikipedia</cite></a></footer>
+        </blockquote>
+    </div>
 </div>
 @stop
