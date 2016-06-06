5411dcb4d3000d4c91c41e6c39b781a9bf7898a4
Xavier Mabras <xavier.mabras@holaluz.com>
2016-06-06 23:43:26 +0200

    Removed cups Controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,30 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Cups;
 
-Route::get('/', 'CupsController@index');
-Route::get('cups', 'CupsController@index');
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::get('/', function () {
+     return view('cups/index', ['cups' => (new Cups())->generate()]);
+});
+Route::get('cups', function() {
+    return view('cups/index', ['cups' => (new Cups())->generate()]);
 });
