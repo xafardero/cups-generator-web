abf514665cd30cce14daafc56819b4d6f5a78e44
xaf <xavier.mabras@protonmail.ch>
2016-06-07 13:07:08 +0200

    Change title and metas

--- resources/views/cups/index.blade.php
+++ resources/views/cups/index.blade.php
@@ -1,10 +1,9 @@
 @extends ('layout')
-
 @section ('content')
 <div class="row">
     <div class="col-md-6 col-md-offset-3 ">
-        <h1 class="text-center">CUPS</h1>
-        <h2 class="text-center">Generador aleatorio</h2>
+        <h1 class="text-center">Código Universal del Punto de Suministro</h1>
+        <h2 class="text-center">Generador aleatorio de CUPS</h2>
         <form>
             <div class="form-group">
                 <input name="cups" type="text" value="{!! $cups; !!}" class="form-control text-center">
@@ -15,9 +14,17 @@
         </form>
         <blockquote id="wiki">
             <p>
-                El CUPS (Código Universal del Punto de Suministro), en España, es un código único e identificador de un punto de suministro de energía, ya sea de electricidad o gas canalizado.             Se creó con el objetivo de identificar cada suministro ante compañías suministradoras, distribuidoras, comercializadoras, administración estatal, comunidades autónomas.. etc. Actualmente es muy útil  (...)
+                El CUPS (Código Universal del Punto de Suministro), en España, es un código único e 
+                identificador de un punto de suministro de energía, ya sea de electricidad o gas canalizado.
+                Se creó con el objetivo de identificar cada suministro ante compañías suministradoras, distribuidoras, 
+                comercializadoras, administración estatal, comunidades autónomas.. etc.
+                Actualmente es muy útil  (...)
             </p>
-            <footer> <a href="https://es.wikipedia.org/wiki/C%C3%B3digo_Universal_de_Punto_de_Suministro"><cite title="Source Title">Wikipedia</cite></a></footer>
+            <footer>
+                <a rel="nofollow" href="https://es.wikipedia.org/wiki/C%C3%B3digo_Universal_de_Punto_de_Suministro">
+                    <cite title="Source Title">Wikipedia</cite>
+                </a>
+            </footer>
         </blockquote>
     </div>
 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Tools</title>
+        <meta name="description" content="Generador de cups eléctrico aleatorio">
+        <title>Generador de CUPS</title>
         <!-- Fonts -->
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
         <!-- Latest compiled and minified CSS -->
