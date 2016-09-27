e0b2aeb675570da64ec8ecd3dfda03c70bc63c32
xaf <xavier.mabras@protonmail.ch>
2016-09-28 00:15:26 +0200

    Upgraded to Laravel 5.3

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use CupsGenerator\Cups;
+
+Route::get('/', function () {
+     return view('cups/index', ['cups' => (new Cups())->generate()]);
+});
+Route::get('cups', function() {
+    return view('cups/index', ['cups' => (new Cups())->generate()]);
+});
