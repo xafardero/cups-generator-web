0575b14df0fd46a20a7b3442b0cffa6d322320f6
xaf <xavier.mabras@protonmail.ch>
2016-10-24 21:56:17 +0200

    Add an error to UnitTest to test jenkins

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Generador aleatorio de CUPS');
+             ->see('Generador aleatorio de CUPS ERROR');
     }
 }
